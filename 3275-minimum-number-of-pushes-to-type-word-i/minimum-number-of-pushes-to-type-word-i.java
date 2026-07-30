cadc0bd35482fd4d445ca356c49732efc5efcc8b
Ambati Vikas Goud <vikasgoudambati@gmail.com>
2026-07-30 15:00:29 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (79.43%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int totalPushes = 0;
+        
+        
+        for (int i = 0; i < word.length(); i++) {
+            
+            totalPushes += (i / 8) + 1;
+        }
+        
+        return totalPushes;
+    }
+}
