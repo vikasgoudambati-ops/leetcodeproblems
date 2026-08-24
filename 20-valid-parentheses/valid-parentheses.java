4595aa31973c90275a2ddb53068063447a0c32eb
Ambati Vikas Goud <vikasgoudambati@gmail.com>
2026-08-24 12:10:48 +0530

    Time: 3 ms (86.07%) | Memory: 43.3 MB (57.74%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.isEmpty()) return false;
+                char top = st.pop();
+                if (c== ')' && top != '(') return false;
+                if (c== '}' && top != '{') return false;
+                if (c== ']' && top != '[') return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
