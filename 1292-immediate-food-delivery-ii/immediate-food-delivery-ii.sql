0c2cb022e8879476bbb6adaa77e963376ebad8bd
Ambati Vikas Goud <vikasgoudambati@gmail.com>
2026-09-04 13:44:51 +0530

    Time: 650 ms (73.17%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT ROUND(100 * AVG(order_date = customer_pref_delivery_date), 2) AS immediate_percentage
+FROM Delivery WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+);
